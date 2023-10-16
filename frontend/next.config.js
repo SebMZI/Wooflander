4e889179aa69f527c08f01411a83fba52ed8712f
SebMZI <mrgy.sebastien@gmail.com>
2023-10-16 16:38:56 +0200

    feat(frontend): add home page Add a background video for more life on the website

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+// next.config.js
+const path = require("path");
+
+module.exports = {
+  ...nextConfig,
+
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next",
+            name: "static/videos/[name].[ext]", // Répertoire de sortie pour les vidéos
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
